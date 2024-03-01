993d05e06c9ccfccc6d03e168bd22f33d8b6695e
Kumari <shruti.b.kumari@capgemini.com>
2024-03-01 22:26:59 +0530

    First Stage Of Project

---
+++ OnlineCabBookingSystem/CabApp.Entities/Common/BaseObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabApp.Entities.Common
+{
+    public abstract class BaseObject
+    {
+        [Key]
+        public int ID { get; set; }
+        public DateTime Create { get; set; }
+
+        public DateTime Update { get; set; }
+
+        public DateTime Delete { get; set; }
+
+    }
+}
---
+++ OnlineCabBookingSystem/CabApp.Entities/VehicleDetails/Category.cs
@@ -0,0 +1,15 @@
+﻿using CabApp.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabApp.Entities.VehicleDetails
+{
+    public class Category : BaseObject
+    {
+        
+    }
+}

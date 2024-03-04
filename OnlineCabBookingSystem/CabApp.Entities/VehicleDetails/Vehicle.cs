c8919079986c4b451961e5c9ab19903842cd7687
Kumari <shruti.b.kumari@capgemini.com>
2024-03-04 15:51:51 +0530

    First Stage o Project

---
+++ OnlineCabBookingSystem/CabApp.Entities/VehicleDetails/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabApp.Entities
+{
+    public class Vehicle : BaseObject
+    {
+        [Required]
+        public string VehicleNumber { get; set; }
+
+        [Required]
+        public string Brand { get; set; }
+
+        [Required]
+        public string Color { get; set; }
+
+        [Required]
+        public VehicleType VehicleType { get; set; }
+
+        [Required]
+        public VehicleCategory Category { get; set; }
+
+    }
+}

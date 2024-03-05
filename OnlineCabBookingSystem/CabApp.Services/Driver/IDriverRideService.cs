ee045209c293436342f49bfd00685e2da9863d69
Shruthii-Kumarii <shruthii.kumarii@gmail.com>
2024-03-05 14:14:44 +0530

    Remove Error

--- OnlineCabBookingSystem/CabApp.Services/Driver/IDriverRideService.cs
+++ OnlineCabBookingSystem/CabApp.Services/Driver/IDriverRideService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CabApp.Services.Driver
+namespace CabApp.Services
 {
     public interface IDriverRideService
     {

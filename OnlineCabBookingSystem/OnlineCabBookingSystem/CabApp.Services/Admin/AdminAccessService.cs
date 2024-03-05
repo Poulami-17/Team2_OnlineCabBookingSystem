21ddd92900c2008208cdac6e4d95c0dbc063e9bf
Shruthii-Kumarii <shruthii.kumarii@gmail.com>
2024-03-05 12:19:35 +0530

    Added New Files

---
+++ OnlineCabBookingSystem/OnlineCabBookingSystem/CabApp.Services/Admin/AdminAccessService.cs
@@ -0,0 +1,58 @@
+﻿using CabApp.Data;
+using CabApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabApp.Services
+{
+    public class AdminAccessService : IAdminAccessService
+    {
+        private readonly CabAppDbContext _dbContext;
+        public AdminAccessService(CabAppDbContext _dbContext)
+        {
+            this._dbContext = _dbContext;
+        }
+
+        public List<Claim> AdminSignIn(DriverSignInRequest request)
+        {
+           var admin = _dbContext.Admins.FirstOrDefault(d=>d.Email== request.Email && d.Password == request.Password);
+            if(admin == null)
+            {
+                throw new AuthenticationException("Login Failed");
+            }
+            var result = new List<Claim>
+            {
+                new Claim("Email",admin.Email),
+                new Claim(ClaimTypes.NameIdentifier,admin.Name),
+            };
+            return result;
+        }
+
+        public Admin AdminSignUp(AdminSignUpRequest request)
+        {
+            if (_dbContext.Admins.Any(d => d.Email == request.Email))
+                throw new DuplicateEmailException("Email already exist");
+
+            var admin = new Admin();
+            admin.Name = request.Name;
+            admin.Email = request.Email;
+            admin.Password = request.Password;
+            admin.PhoneNumber = 0;
+            admin.UserName = request.UserName;
+            
+            
+            
+
+            _dbContext.Admins.Add(admin);
+            _dbContext.SaveChanges();
+
+            return admin;
+        }
+    }
+}
---
+++ OnlineCabBookingSystem/OnlineCabBookingSystem/CabApp.Services/Driver/IDriverAccessService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using CabApp.Entities;
+
+namespace CabApp.Services
+{
+    public interface IDriverAccessService
+    {
+        Driver DriverSignUp(DriverSignUpRequest request);
+
+        List<Claim> DriverSignIn(DriverSignInRequest request);
+    }
+}

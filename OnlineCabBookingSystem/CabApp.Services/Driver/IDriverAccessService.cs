f3742627db63fca54102e88531c6a2e1ab062396
MeghanaH01 <meghana.h@capgemini.com>
2024-03-05 10:51:29 +0530

    Added driver access

---
+++ OnlineCabBookingSystem/CabApp.Services/Driver/IDriverAccessService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using CabApp.Entities;
+
+namespace CabApp.Services
+{
+    public interface IDriverAccessService
+    {
+        Driver SignUp(SignUpRequest request);
+
+        List<Claim> SignIn(SignInRequest request);
+    }
+}

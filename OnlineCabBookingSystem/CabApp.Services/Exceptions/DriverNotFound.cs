6d16addcfcb9de017ee00c6de18afe4d1c6ab73e
Shruthii-Kumarii <shruthii.kumarii@gmail.com>
2024-03-07 10:04:40 +0530

    addException

---
+++ OnlineCabBookingSystem/CabApp.Services/Exceptions/DriverNotFound.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabApp.Services.Exceptions
+{
+    internal class DriverNotFound
+    {
+    }
+}
